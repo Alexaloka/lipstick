00d4d78173ed4266db2b2151ac47997ecf404a4f
Alexaloka <103824973+Alexaloka@users.noreply.github.com>
2023-03-23 13:47:20 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function prelosd(){
+
+}
+function setup(){
+    canvas = createCanvas(300,300);
+    canvas.center();
+}
+function draw(){
+
+}
+function take_snapshot()
+{
+    save('myFilterImage.png');
+}
